e288b66e23713e007be05afe817837088990c021
jgomez89 <justin.gomez3789@gmail.com>
2020-05-17 11:22:15 -0700

    added goals to script, to be filled in later

--- src/Compressor.py
+++ src/Compressor.py
@@ -5,4 +5,28 @@ img = Image.open('../images/plains.jpeg')
 array = np.array(img)
 print(array.shape)      # (170, 256, 3)
 img = Image.fromarray(array)
+print(array)
 img.save('../images/export/testrgb.jpeg')
+
+
+# Take string or image as input
+    # Create array representation of rgb_image
+
+## Image -> Binary
+# Hash similar valued pixels to same color (decrease resolution)
+    # idk: "truncate" rgb values
+    # (or) make 3x3 pixel set be represented by the middle pixel
+
+# Huffman Algorithm
+    # Make frequency dictionary
+    # Make heap
+    # Merge Nodes and build tree
+    # Make codes
+    # Encode pixels
+    # Pad encodded text
+    # Make byte array
+# Output the byte array to binary file
+    # Maybe: show improvement by comparing it to a naive conversion from image to binary
+
+
+## Binary-> Image
