abccaad8603df54e4a3dd06d2d31dc9e2d88c04c
jgomez89 <justin.gomez3789@gmail.com>
2020-05-19 22:16:12 -0700

    changed func inc_freq -> degradation, finished funct along w/ testing func

--- src/HuffmanCoding.py
+++ src/HuffmanCoding.py
@@ -26,6 +26,8 @@ class HeapNode:
 
 
 
+
+
 class Huffman:
 
     # Constructor
@@ -38,22 +40,85 @@ class Huffman:
         self.rows = self.img_array.shape[0]
         self.colomns = self.img_array.shape[1]
 
-        #
+        # Structures
         self.heap = []
         self.encodded = {}
 
 
 
 
-    # Compression functions
+    ## Functions for running tests
+    def test_img(self,filename):
+        self.degredation()
+        img = Image.fromarray(self.img_array)
+        if filename.endswith('.jpg'):
+            new_filename = filename[:len(filename)-4]+'_degredated.jpg'
+            img.save('../images/export/' + new_filename)
+        else:
+            img.save('../images/export/testrgb.jpg')
+
+
+
+
+    ## Compression functions
 
     # Increase the frequency of colors / image loss
-    def increase_freq(self):
-        # Take each nxn pixels
-        # Set the color of each colomn to average color of the colomn
+    def degredation(self):
+
+        # Precision variables
+        # (MAYBE: make precision variables a function of the size of the image)
+        reso_size = 4
+        num_diff_vals = 50
+
+
+        total_num_colors = num_diff_vals^3
+        prec_size = 255.0 / num_diff_vals
+        remaining_rows = self.rows % reso_size
+
+
+        # Set the color of each (reso_size x 1) colomn to average color of that colomn
         # Truncate the values by integer division
+        for i in range(0,self.rows-remaining_rows,reso_size):
+
+            for j in range(0,self.colomns):
+                valRed = np.array([self.img_array[k][j][0] for k in range(i,i+reso_size)])
+                avgRed = valRed.mean()
+
+                valGreen = np.array([self.img_array[k][j][1] for k in range(i,i+reso_size)])
+                avgGreen = valGreen.mean()
+
+                valBlue = np.array([self.img_array[k][j][2] for k in range(i,i+reso_size)])
+                avgBlue = valBlue.mean()
+
+                avgRGB = [avgRed,avgGreen,avgBlue]
+                avgRGB[:] = [int(x) - int(x)%prec_size for x in avgRGB]
+
+                for l in range(i,i+reso_size):
+                    self.img_array[l][j] = avgRGB
+
+        # If any rows are left at bottom of image, then continue
+        if (not remaining_rows == 0):
+
+            for j in range(0,self.colomns):
+                valRed = np.array([self.img_array[k][j][0] for k in range(i,self.rows)])
+                avgRed = valRed.mean()
+
+                valGreen = np.array([self.img_array[k][j][1] for k in range(i,self.rows)])
+                avgGreen = valGreen.mean()
+
+                valBlue = np.array([self.img_array[k][j][2] for k in range(i,self.rows)])
+                avgBlue = valBlue.mean()
+
+                avgRGB = [avgRed,avgGreen,avgBlue]
+                avgRGB[:] = [int(x) - (x)%prec_size for x in avgRGB]
+
+                for l in range(i,self.rows):
+                    self.img_array[l][j] = avgRGB
 
 
+
+
+    def fill_freq_dict(self,):
         # an_array = np.array([[1,2],[3,4]])
         # another_array = np.array([[1,2],[3,4]])
         #
@@ -63,30 +128,6 @@ class Huffman:
         # print(equal_arrays)
         # OUTPUT
         # True
-
-
-        block_size = 8
-
-        num_diff_vals = 9
-        num_of_colors = num_diff_vals^3
-        prec_size = 255.0 / num_diff_vals
-
-        for i in range(0,self.rows,block_size):
-
-            for j in range(0,self.colmns):
-
-                avgRed = self.img_array[i:i+block_size][j][0].mean(axis=0)
-                self.img_array[i:i+block_size][j][0] = avgRed
-
-                avgGreen = self.img_array[i:i+block_size][j][1].mean(axis=0)
-                self.img_array[i:i+block_size][j][1] = avgGreen
-
-                avgBlue = self.img_array[i:i+block_size][j][2].mean(axis=0)
-                self.img_array[i:i+block_size][j][2] = avgBlue
-        
-
-
-    def fill_freq_dict(self,):
 
         frequency = {}
 
@@ -121,7 +162,9 @@ class Huffman:
     def compress(self):
             pass
 
-    # Decompression
+
+
+    ## Decompression
 
     def decompress(self,path):
         # img = Image.fromarray(self.img_array)
---
+++ src/tryme.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import Image
+from HuffmanCoding import Huffman
+
+
+def main():
+
+    # Enter whatever .jpg file from images folder
+    # Expect degredated image in exported
+
+    filename = 'plains.jpg'
+
+    path = '../images/' + filename
+    h = Huffman(path)
+    h.test_img(filename)
+
+main()
